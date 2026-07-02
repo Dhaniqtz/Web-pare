4d48d095b0cf4e280d8866ed67caccade4654575
Dhaniqtz <Chirandanitha652@gmail.com>
2026-07-02 12:52:39 +0530

    Change email and password in auth object
    Updated authentication credentials for mega.js.

--- mega.js
+++ mega.js
@@ -1,7 +1,7 @@
 const mega = require("megajs");
 const auth = {
-    email: '',
-    password: '',
+    email: 'chirandanitha652@gmail.com',
+    password: 'Danitha@164',
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 }
 
