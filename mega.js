7196d8acfb8687bdc3f841451bd99ac2296a40c4
Dhaniqtz <Chirandanitha652@gmail.com>
2026-07-12 21:20:00 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,6 +1,6 @@
 const mega = require("megajs");
 const auth = {
-    email: 'chirandanitha652@gmail.com',
+    email: 'acc623576@gmail.com',
     password: 'Danitha@164',
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 }
